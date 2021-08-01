849a96bff97c43de94824f341690a78fb9521a52
mafei <mafei.dev@gmail.com>
2021-08-01 16:30:32 +0530

    main class updated.

--- src/main/java/com/mafei/example/SpringLombokMapstructApplication.java
+++ src/main/java/com/mafei/example/SpringLombokMapstructApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestApiVersioningApplication {
+public class SpringLombokMapstructApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRestApiVersioningApplication.class, args);
+        SpringApplication.run(SpringLombokMapstructApplication.class, args);
     }
 
 }
